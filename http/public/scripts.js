5dbeed193971327ace62b616ab483942a2eb1adc
Izael silva <izaell.universitario@outlook.com>
2020-04-24 18:49:27 -0300

    Structured alert messages

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -2,6 +2,13 @@ const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 const form = document.querySelector("form")
 
+async function load(){
+    const res = await fetch("http://127.0.0.1:3000/").then((data)=>data.json())
+    res.urls.map(({ name, url }) => addElement({ name, url }))
+}
+
+load()
+
 function addElement({ name, url }){
     const li = document.createElement("li")
     const a = document.createElement("a")
@@ -32,11 +39,11 @@ form.addEventListener("submit", (event) => {
         return alert('Preencha os campos!!')
     }
 
-    const [name, url] = value.split(", ")
+    const [name, url] = value.split(" - ")
     if(!url){
-        return alert('Formate o texto da maneira correta!!')
+        return alert(`Coloque o texto exatamente como está no exemplo, respeitando os espaços e traços!! \n Exemplo: < url name - https://www.exemplo.com.br >`)
     }
-    if(!/^http/ .test(url)){
+    if(!/^https/.test(url)){
         return alert('Digite a URL da maneira correta!!')
     }
 
